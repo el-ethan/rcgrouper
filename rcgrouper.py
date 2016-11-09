bf8c50f8f0054c84e6cd17395e70c3db3e2c81dd
el-ethan <elethan@gmail.com>
2016-11-09 09:41:59 -0600

    Add cleanup function, ability to scrape multiple sites

--- rcgrouper.py
+++ rcgrouper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import textwrap
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import smtplib
 import ConfigParser
@@ -9,6 +9,12 @@ from bs4 import BeautifulSoup
 import requests
 
 # TODO clear out matching links list periodically
+# TODO differentiate between sold items, wanted items, etc.
+
+SITES = {
+    'multirotor': 'http://www.rcgroups.com/aircraft-electric-multirotor-fs-w-733/',
+    'fpv': 'https://www.rcgroups.com/fpv-equipment-fs-w-710/',
+}
 
 ROOT_URL = 'http://www.rcgroups.com/forums/'
 PROJECT_DIR = os.path.dirname(__file__)
@@ -67,9 +73,36 @@ class Page(object):
                 f.write(ROOT_URL + m.attrs['href'] + '\n')
 
 
+def cleanup_matches(config):
+    '''Clean up old matches and set new expiration date if necessary
+
+    If the current date is >= the expiration date in the config file,
+    empty matches.txt file and update config with new expiration date
+    one week in the future.
+    '''
+    _now = datetime.now()
+    date_fmt = '%Y-%m-%d'
+    exp = config.get('rcgrouper', 'match_expiration')
+    exp_dt = datetime.strptime(exp, date_fmt)
+    if _now >= exp_dt:
+        open(MATCH_FILE, 'w').close()
+        new_exp = _now + timedelta(weeks=1)
+        config.set('rcgrouper', 'match_expiration', new_exp.strftime(date_fmt))
+        with open('.grouper', 'wb') as configfile:
+            config.write(configfile)
+
+
 if __name__ == '__main__':
-    r = requests.get('http://www.rcgroups.com/aircraft-electric-multirotor-fs-w-733/')
+
     cfg = ConfigParser.RawConfigParser()
     cfg.read(CONFIG_FILE)
-    page = Page(r.text, config=cfg)
+    for_sale_only = True if cfg.get('rcgrouper', 'for_sale_only') == 'true' else False
+    suffix = '?prefixid=For_Sale_def_' if for_sale_only else ''
+    _html = ''
+    sites_to_check = cfg.get('rcgrouper', 'sites_to_check').split(',')
+    for site in sites_to_check:
+        _html += requests.get(SITES[site] + suffix).text
+
+    page = Page(_html, config=cfg)
     page.email_matching_posts()
+    cleanup_matches(cfg)
